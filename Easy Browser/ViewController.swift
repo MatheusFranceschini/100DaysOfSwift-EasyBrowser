6eeeb87e633c5b5a98d567c9c71e4e609cce3704
MatheusFranceschini <masuzena@gmail.com>
2024-08-27 12:32:20 -0300

    Initial Commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Matheus Franceschini on 27/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
